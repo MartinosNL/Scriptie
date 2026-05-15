d3999916e3adb86c44944d6e2ab71ee113d05346
Martijn Huls <hulsmj@gmail.com>
2026-05-15 03:29:14 +0200

    small update to the evalutation script

--- evaluation/evaulate.py
+++ evaluation/evaulate.py
@@ -15,11 +15,7 @@ def main():
     y_true = [item["runtime_bracket"] for item in data]
     y_pred = [item["llm_prediction"] for item in data]
 
-    print(y_true)
-    print('-'*50)
-    print(y_pred)
     print(metrics.confusion_matrix(y_true, y_pred, labels=['<3ms', '3-4ms', '4-5ms', '5-10ms', '10ms-1s', '>1s']))
-
     print(metrics.classification_report(y_true, y_pred))
 
 if __name__ == "__main__":
