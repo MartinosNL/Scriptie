147b76963d1a4f4671ccb5894c2c66ed72645cc6
Martijn Huls <hulsmj@gmail.com>
2026-06-05 13:17:54 +0200

    fixed missing AST

--- data_and_generation/make_ast.py
+++ data_and_generation/make_ast.py
@@ -2,19 +2,22 @@ import ast
 import os
 import json
 
-path = "C:\\Users\\hulsm\\Documents\\Scriptie\\data_and_generation\\used_data\\"
+path = "C:\\Users\\hulsm\\Documents\\Scriptie\\data_and_generation\\combined_final_data\\"
 
 def main():
-    input_file = os.path.join(path, "clean_combined\\clean_combined.json")
-    output_file = os.path.join(path, "ast_combined\\clean_combined_ast.json")
+    input_file = os.path.join(path, "combined_final_data.json")
+    output_file = os.path.join(path, "combined_final_data.json")
     with open(input_file, "r", encoding="utf-8") as f:
         data = json.load(f)
     for item in data:
+        ast_item = item["ast"]
         code = item["code"]
+        if ast_item is not None:
+            continue  # Skip if ast is not None
         try:
             item["ast"] = ast.dump(ast.parse(code))
         except Exception as e:
-            print(f"Error parsing code: {e}")
+            print(f"Error parsing code: {item['id']}: {e}")
             item["ast"] = None
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
